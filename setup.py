45979ad26861ba89bb6035787519296a20c8ccbd
143biswajit <gochhayatbiswajit253@gmail.com>
2024-06-23 19:54:49 +0530

    1st cimmite

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+
+def get_requirments()->list[str]
+    requirment_list:[str]=[]
+    return requirment_list
+
+
+setup(
+name='sensor',
+version="0.0.1",
+author="143biswajit",
+authour_email="gochhayatbiswajit253@gmail.com",
+packages=find_packages(),
+install_requires=get_requirments()
+
+
+
+)
