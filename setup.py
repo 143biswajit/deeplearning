db9cad134ea6f3489ecd28de533fa8d1873297c9
143biswajit <gochhayatbiswajit253@gmail.com>
2024-06-27 23:14:24 +0530

    exception and logging file run

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import find_packages,setup
+from typing import List
 
 
-def get_requirments()->list[str]
-    requirment_list:[str]=[]
+def get_requirments()->List[str]:
+    """
+    This function will return list of requirments
+    
+    """
+    requirment_list: List[str]=[]
+
+    """
+
+    write a code to read requirments.txt file and append each requirments in requirment_list variable.
+    """
     return requirment_list
 
 
@@ -16,4 +26,4 @@ install_requires=get_requirments()
 
 
 
-)
+) 
